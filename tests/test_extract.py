db5d842051f2f9d40379aacb4f163505e099b5c2
Patrick Scheibe <patrick@halirutan.de>
2023-10-04 01:49:31 +0200

    Add test case

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,8 +1,12 @@
 from matdocmd.extract import DocumentationExtractor
+from matdocmd.types import FunctionDocumentationResult
 from . import test_dir_path
 
 
 def test_extract():
     file = test_dir_path / "reconstruction.m"
-    e = DocumentationExtractor(file.absolute().__str__()).result
-    print(e)
+    e: FunctionDocumentationResult = DocumentationExtractor(file.absolute().__str__()).result
+    print("\n\n")
+    print(f"Type Info: {e.type}")
+    print(f"Function Signature: {e.signature}")
+    print(f"Doc String: \n{e.doc_comment}")
